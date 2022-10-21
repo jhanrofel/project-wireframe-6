94a9c43934c65c334b0b812e5698b464b0a25b2c
Rofel <jhanrofel@gmail.com>
2022-10-21 12:16:39 +0800

    removed unnecessary methods and variables on share-to controller

--- server/src/controllers/share-to.controller.ts
+++ server/src/controllers/share-to.controller.ts
@@ -1,18 +1,9 @@
-import {
-  Count,
-  CountSchema,
-  Filter,
-  FilterExcludingWhere,
-  repository,
-  Where,
-} from '@loopback/repository';
+import {repository} from '@loopback/repository';
 import {
   post,
   param,
   get,
   getModelSchemaRef,
-  patch,
-  put,
   del,
   requestBody,
   response,
@@ -23,7 +14,7 @@ import {ShareToRepository} from '../repositories';
 export class ShareToController {
   constructor(
     @repository(ShareToRepository)
-    public shareToRepository : ShareToRepository,
+    public shareToRepository: ShareToRepository,
   ) {}
 
   @post('/share-tos')
@@ -47,17 +38,6 @@ export class ShareToController {
     return this.shareToRepository.create(shareTo);
   }
 
-  @get('/share-tos/count')
-  @response(200, {
-    description: 'ShareTo model count',
-    content: {'application/json': {schema: CountSchema}},
-  })
-  async count(
-    @param.where(ShareTo) where?: Where<ShareTo>,
-  ): Promise<Count> {
-    return this.shareToRepository.count(where);
-  }
-
   @get('/share-tos')
   @response(200, {
     description: 'Array of ShareTo model instances',
@@ -70,29 +50,8 @@ export class ShareToController {
       },
     },
   })
-  async find(
-    @param.filter(ShareTo) filter?: Filter<ShareTo>,
-  ): Promise<ShareTo[]> {
-    return this.shareToRepository.find(filter);
-  }
-
-  @patch('/share-tos')
-  @response(200, {
-    description: 'ShareTo PATCH success count',
-    content: {'application/json': {schema: CountSchema}},
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(ShareTo, {partial: true}),
-        },
-      },
-    })
-    shareTo: ShareTo,
-    @param.where(ShareTo) where?: Where<ShareTo>,
-  ): Promise<Count> {
-    return this.shareToRepository.updateAll(shareTo, where);
+  async find(): Promise<ShareTo[]> {
+    return this.shareToRepository.find();
   }
 
   @get('/share-tos/{id}')
@@ -104,40 +63,8 @@ export class ShareToController {
       },
     },
   })
-  async findById(
-    @param.path.string('id') id: string,
-    @param.filter(ShareTo, {exclude: 'where'}) filter?: FilterExcludingWhere<ShareTo>
-  ): Promise<ShareTo> {
-    return this.shareToRepository.findById(id, filter);
-  }
-
-  @patch('/share-tos/{id}')
-  @response(204, {
-    description: 'ShareTo PATCH success',
-  })
-  async updateById(
-    @param.path.string('id') id: string,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(ShareTo, {partial: true}),
-        },
-      },
-    })
-    shareTo: ShareTo,
-  ): Promise<void> {
-    await this.shareToRepository.updateById(id, shareTo);
-  }
-
-  @put('/share-tos/{id}')
-  @response(204, {
-    description: 'ShareTo PUT success',
-  })
-  async replaceById(
-    @param.path.string('id') id: string,
-    @requestBody() shareTo: ShareTo,
-  ): Promise<void> {
-    await this.shareToRepository.replaceById(id, shareTo);
+  async findById(@param.path.string('id') id: string): Promise<ShareTo> {
+    return this.shareToRepository.findById(id);
   }
 
   @del('/share-tos/{id}')
