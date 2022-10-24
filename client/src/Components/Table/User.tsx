0a7b4971af1f5e7a8d0744ccd03af8bf3864289d
Rofel <jhanrofel@gmail.com>
2022-10-24 20:25:28 +0800

    manage user components UI, state and props are ready

--- client/src/Components/Table/User.tsx
+++ client/src/Components/Table/User.tsx
@@ -1,9 +1,126 @@
-import React from 'react';
+import React, { ElementRef, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Table from "react-bootstrap/Table";
+import BsButton from "react-bootstrap/Button";
 
-const User: React.FC = () => {
+import ConfirmModal from "../Modal/ConfirmModal";
+import TableHeader from "../TableHeader";
+import EmptyRow from "../EmptyRow";
+
+// import { ApiDeleteUser } from "../../Utilitites/Api";
+// import { LoggedIn } from "../../Utilitites/LoggedIn";
+// import { useDispatch, useSelector } from "react-redux";
+// import { deleteUser } from "../../Utilitites/Slice/UserSlice";
+
+type AppProps = {
+  data: {
+    name: string;
+    headers: Array<{
+      label:string;
+      width:string;
+    }>;
+    minRows: number;
+    numCols: number;
+  };
+};
+
+const User = ({ data }: AppProps) => {
+  // const dispatch = useDispatch();
+  // const loggedIn = LoggedIn();
+  const navigate = useNavigate();
+  // const lists = useSelector((state) => state.user.data);
+  const lists: string[] = [];
+  const [deleteUserId, setDeleteUserId] = useState("");
+  const [userDeleted, setUserDeleted] = useState("");
+  const [show, setShow] = useState(false);
+  const handleClose = ():void => setShow(false);
+  const modalData = {
+    header: "Confirm User Deletion",
+    body: "Are you sure ?",
+    image: "",
+    confirmValue: "ok",
+    cancelValue: "Cancel",
+  };
+
+  const [users, setUsers] = useState([]);
+  // useEffect(() => {
+  //   setUsers(lists);
+  // }, [lists,userDeleted]);
+
+  const editAction = (id: string) => {
+    return (
+      <BsButton
+        className="btn-action"
+        variant="link"
+        onClick={() => navigate("/edit-user", { state: id })}
+      >
+        Edit
+      </BsButton>
+    );
+  };
+
+  const deleteAction = (id: string) => {
+    return (
+      <BsButton
+        className="btn-action"
+        variant="link"
+        onClick={() => onDeleteActionHandler(id)}
+      >
+        Delete
+      </BsButton>
+    );
+  };
+
+  const deleteDisable = () => {
+    return (
+      <BsButton className="btn-disable" variant="link">
+        Delete
+      </BsButton>
+    );
+  };
+
+  const onDeleteActionHandler = (id: string) => {
+    setDeleteUserId(id);
+    setShow(true);
+  };
+
+  const onDeleteHandler = (): void => {
+    // dispatch(deleteUser(deleteUserId)).then(() => {
+    //   setUserDeleted(deleteUserId);
+    //   setShow(false);
+    // });
+  };
   return (
-    <div>User</div>
-  )
-}
+    <div className="table-container">
+      <Table striped>
+        <TableHeader headers={data.headers} />
+        <tbody>
+          {users.map((user, i) => (
+            <tr key={i}>
+              {/* <td className="td-left">{user.fullname}</td>
+              <td className="td-border">{user.email}</td>
+              <td>
+                {editAction(user._id)}|
+                {loggedIn.userId !== user._id
+                  ? deleteAction(user._id)
+                  : deleteDisable()}
+              </td> */}
+            </tr>
+          ))}
+          <EmptyRow
+            count={data.minRows - lists.length}
+            colCount={data.numCols}
+          />
+        </tbody>
+      </Table>
+      <ConfirmModal
+        show={show}
+        handleClose={handleClose}
+        modalData={modalData}
+        onConfirmHandler={onDeleteHandler}
+      />
+    </div>
+  );
+};
 
 export default User;
--- client/src/Pages/User/ManageUser.tsx
+++ client/src/Pages/User/ManageUser.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React from "react";
+
+import TableUser from "../../Components/Table/User";
+import TableTitle from "../../Components/TableTitle";
+
+// import { useDispatch,useSelector } from "react-redux";
+// import { fetchUsers } from "../../Utilitites/Slice/UserSlice";
 
 const ManageDocument: React.FC = () => {
+  // const dispatch = useDispatch();
+
+  const users = {
+    name: "users",
+    headers: [
+      { label: "Name", width: "40%" },
+      { label: "User Email ID", width: "35%" },
+      { label: "", width: "35%" },
+    ],
+    minRows: 10,
+    numCols: 3,
+  };
+  // const lists = useSelector((state) => state.user.data);
+
+  // useEffect(() => {
+  //   dispatch(fetchUsers());
+  // }, [dispatch]);
   return (
-    <div>Manage Document</div>
-  )
-}
+    <>
+      <TableTitle text="Users" />
+      <TableUser data={users}/>
+    </>
+  );
+};
 
 export default ManageDocument;
