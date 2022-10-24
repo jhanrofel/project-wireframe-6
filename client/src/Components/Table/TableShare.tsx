77f8fa8ea21be258d00c202b945c9da2d56384e7
Rofel <jhanrofel@gmail.com>
2022-10-25 07:26:51 +0800

    share file page components UI,state,props and navigation ready

--- client/src/Components/Table/TableShare.tsx
+++ client/src/Components/Table/TableShare.tsx
@@ -1,8 +1,85 @@
-import React from 'react';
+import React, { useState } from "react";
+import Table from "react-bootstrap/Table";
+import BsButton from "react-bootstrap/Button";
 
-const TableShare: React.FC = () => {
+import ConfirmModal from "../Modal/ConfirmModal";
+import TableHeader from "../TableHeader";
+import EmptyRow from "../EmptyRow";
+import { type } from "@testing-library/user-event/dist/type";
+// import { useDispatch } from "react-redux";
+// import { deleteShare } from "../../Utilitites/Slice/ShareSlice";
+
+type AppProps = {
+  data: {
+    headers: Array<{
+      label:string;
+      width:string;
+    }>;
+    minRows: number;
+    numCols: number;
+  }
+  shareLists: string[];
+}
+
+const TableShare = ({ data, shareLists } : AppProps) => {
+  // const dispatch = useDispatch();
+  const [removeShareId, setRemoveShareId] = useState("");
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const modalData = {
+    header: "Confirm From Web Page",
+    body: "Are you sure ?",
+    image: "",
+    confirmValue: "ok",
+    cancelValue: "Cancel",
+  };
+
+  const removeAction = (id:string) => {
+    return (
+      <BsButton
+        className="btn-action"
+        variant="link"
+        onClick={() => onRemoveActionHandler(id)}
+      >
+        Remove
+      </BsButton>
+    );
+  };
+
+  const onRemoveActionHandler = (id:string):void => {
+    setRemoveShareId(id);
+    setShow(true);
+  };
+
+  const oneRemoveHandler = async () => {
+    // await dispatch(deleteShare(removeShareId)).then(() => {
+    //   setShow(false);
+    // });
+  };
   return (
-    <div>Table Share</div>
+    <div className="table-container">
+      <Table striped>
+        <TableHeader headers={data.headers} />
+        <tbody>
+          {shareLists.map((list, i) => (
+            <tr key={i}>
+               {/* <td className="td-left">{list.toUserId.fullname}</td>
+               <td className="td-border-left">{removeAction(list._id)}</td> */}
+            </tr>
+          ))}
+          <EmptyRow
+            count={data.minRows - shareLists.length}
+            colCount={data.numCols}
+          />
+        </tbody>
+      </Table>
+      <ConfirmModal
+        show={show}
+        handleClose={handleClose}
+        modalData={modalData}
+        onConfirmHandler={oneRemoveHandler}
+      />
+    </div>
   )
 }
 
--- client/src/Pages/UploadFile/ShareFile.tsx
+++ client/src/Pages/UploadFile/ShareFile.tsx
@@ -1,8 +1,92 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import BsButton from "react-bootstrap/Button";
+
+import TableShare from "../../Components/Table/TableShare";
+import TableTitle from "../../Components/TableTitle";
+
+// import { LoggedIn } from "../../Utilitites/LoggedIn";
+// import { useDispatch, useSelector } from "react-redux";
+// import {
+//   fetchUserToShare,
+//   fetchShares,
+//   postShare,
+// } from "../../Utilitites/Slice/ShareSlice";
 
 const ShareFile: React.FC = () => {
+  // const dispatch = useDispatch();
+  const { state } = useLocation();
+  // const [uploadFile] = useState(state);
+  // const loggedIn = LoggedIn();
+
+  const shareFiles = {
+    headers: [
+      { label: "Shared User", width: "50%" },
+      { label: "Action", width: "50%" },
+    ],
+    minRows: 5,
+    numCols: 2,
+  };
+  // const userToShare = useSelector((state) => state.share.dataUser);
+  // const shareLists = useSelector((state) => state.share.data);
+  const userToShare:string[] = []
+  const shareLists:string[] = []
+  const [selectUser, setSelectUser] = useState("");
+  // const uploadid = uploadFile.id;
+  // const userId = loggedIn.userId;
+
+  // useEffect(() => {
+  //   dispatch(fetchUserToShare({ uploadid, userId }));
+  //   dispatch(fetchShares(uploadid));
+  // }, [dispatch, uploadid, userId]);
+
+  const onShareHandler = async ():Promise<void> => {
+
+    try {
+      if (selectUser === "") throw new Error("Choose user is required.");
+    } catch (error) {
+      // alert(error.message);
+      return;
+    }
+
+    const shareFileData = {
+      // uploadFileId: uploadFile.id,
+      // fromUserId: loggedIn.userId,
+      toUserId: selectUser,
+    };
+
+    // await dispatch(postShare(shareFileData)).then(() => {
+    //   setSelectUser("");
+    // });
+
+  };
+  //${uploadFile.filename}
   return (
-    <div>Share File</div>
+    <>
+      <TableTitle text={`Upload Sharing : `} />
+      <TableShare
+        data={shareFiles}
+        shareLists={shareLists}
+      />
+      <TableTitle text="Add Sharing" />
+      <div className="choose-body">
+        <span> Choose User : </span>
+        <select
+          value={selectUser}
+          onChange={(e) => setSelectUser(e.target.value)}
+        >
+          <option value="">{"--Select--"}</option>
+          {userToShare.map((user, i) => (
+            <option key={i} >
+              {/* value={user._id} {user.fullname} */}
+            </option>
+          ))}
+        </select>
+        <BsButton name="share" variant="light" onClick={onShareHandler}>
+          Share
+        </BsButton>
+      </div>
+    </>
   )
 }
 
