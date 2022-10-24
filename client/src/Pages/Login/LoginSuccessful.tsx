7dc91fc5acbfc5f072c8024db138d61a26a2861a
Rofel <jhanrofel@gmail.com>
2022-10-24 17:18:58 +0800

    login success page UI ready

--- client/src/Pages/Login/LoginSuccessful.tsx
+++ client/src/Pages/Login/LoginSuccessful.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import Header from "../../Components/Header";
+// import { LoggedIn } from "../../Utilitites/LoggedIn";
 
 const LoginSuccessful: React.FC = () => {
+  // const loggedIn = LoggedIn();
   return (
-    <div>Login Successful</div>
+    <div className="container-fluid">
+      <div className="main">
+        <Header text="Login Successful" />
+        <h5>
+          <span>Welcome ! </span>
+          {/* <span>{loggedIn.email}</span> */}
+        </h5>
+      </div>
+    </div>
   )
 }
 
