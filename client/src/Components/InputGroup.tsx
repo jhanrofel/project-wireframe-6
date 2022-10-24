cdb3ac7d6fd475c798cf25a6c50a7feeb6d622bd
Rofel <jhanrofel@gmail.com>
2022-10-24 16:58:38 +0800

    add props and data for input group reusable component

--- client/src/Components/InputGroup.tsx
+++ client/src/Components/InputGroup.tsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import BsInputGroup from "react-bootstrap/InputGroup";
 
-const InputGroup: React.FC = () => {
+type AppProps = {
+  type: string;
+  name: string;
+  label: string;
+  placeholder: string;
+  value: string;
+  error: string;
+  onChangeHandler?: React.ChangeEventHandler<HTMLInputElement>;
+};
+
+const InputGroup = ({
+  type,
+  name,
+  label,
+  placeholder,
+  value,
+  error,
+  onChangeHandler,
+}: AppProps) => {
   return (
-    <div>InputGroup</div>
-  )
-}
+    <div className="reg-form">
+      <Row className="justify-content-md-center">
+        <Col xs={2}>
+          <BsInputGroup.Text>{label}</BsInputGroup.Text>
+        </Col>
+        <Col xs={10}>
+          <Form.Control
+            type={type}
+            name={name}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChangeHandler}
+          />
+          <p>{error}</p>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default InputGroup;
