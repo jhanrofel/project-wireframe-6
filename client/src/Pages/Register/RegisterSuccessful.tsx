b8d5f7ef72e4121957f1b636ee6d16c460311096
Rofel <jhanrofel@gmail.com>
2022-10-24 17:05:05 +0800

    register success page UI and navigation ready

--- client/src/Pages/Register/RegisterSuccessful.tsx
+++ client/src/Pages/Register/RegisterSuccessful.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
+
+import Header from "../../Components/Header";
+import Subheader from "../../Components/Subheader";
+import Button from "../../Components/Button";
 
 const RegisterSuccessful: React.FC = () => {
+  const navigate = useNavigate();
   return (
-    <div>Register Successful</div>
+    <div>
+      <div className="main">
+        <Header text="Registration Successful" />
+        <Subheader text="Thank you for your registration" />
+        <Button
+          name="link"
+          variant="link"
+          text="Click to return to home page"
+          onClick={() => navigate("/")}
+        />
+      </div>
+    </div>
   )
 }
 
