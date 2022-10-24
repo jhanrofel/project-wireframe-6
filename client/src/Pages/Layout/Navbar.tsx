9662138684e25bd401da246af9e0d3e38af30b85
Rofel <jhanrofel@gmail.com>
2022-10-24 17:23:47 +0800

    navigation bar UI and navigation ready

---
+++ client/src/Pages/Layout/Navbar.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+// import { useDispatch } from "react-redux";
+import { NavLink, useLocation } from "react-router-dom";
+
+// import { CookiesRemove } from "../../Utilitites/Cookies";
+// import { LoggedInRemove, LoggedIn } from "../../Utilitites/LoggedIn";
+// import { SocketConnect } from "../../Utilitites/Socket";
+// import { clearUser } from "../../Utilitites/Slice/UserSlice";
+// import { clearChat } from "../../Utilitites/Slice/ChatSlice";
+// import { clearUpload } from "../../Utilitites/Slice/UploadSlice";
+// import { clearShare } from "../../Utilitites/Slice/ShareSlice";
+
+const Navbar: React.FC = () => {
+  // const dispatch = useDispatch();
+  // const loggedIn = LoggedIn();
+  // const socket = SocketConnect();
+  const pathname = useLocation().pathname;
+
+  const onLogout = async () => {
+    //   dispatch(clearUser());
+    //   dispatch(clearChat());
+    //   dispatch(clearUpload());
+    //   dispatch(clearShare());
+    //   LoggedInRemove();
+    //   CookiesRemove();
+    //   socket.emit("send_message", {
+    //     message: {
+    //       userId: { fullname: loggedIn.fullname },
+    //       message: "Logged out...",
+    //       dateSend: "***",
+    //     },
+    //   });
+  };
+  return (
+    <>
+      <div className="nav-bar">
+        <NavLink
+          to="/group-chat"
+          className={({ isActive }) => (isActive ? "nav-selected" : "nav-link")}
+        >
+          Group Chat
+        </NavLink>
+        <NavLink
+          to="/users-list"
+          className={({ isActive }) =>
+            isActive || pathname === "/edit-user" ? "nav-selected" : "nav-link"
+          }
+        >
+          Manage Users
+        </NavLink>
+        <NavLink
+          to="/doc-list"
+          className={({ isActive }) => (isActive || pathname === "/share" ? "nav-selected" : "nav-link")}
+        >
+          Manage Documents
+        </NavLink>
+        <NavLink
+          to="/"
+          className="nav-link"
+          state={{ logout: true }}
+          onClick={onLogout}
+        >
+          Logout
+        </NavLink>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
