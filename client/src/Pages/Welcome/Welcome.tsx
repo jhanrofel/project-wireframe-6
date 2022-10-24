53fa990f7bc8a9628112be77e02e914463e0da56
Rofel <jhanrofel@gmail.com>
2022-10-24 15:44:17 +0800

    welcome component ready

--- client/src/Pages/Welcome/Welcome.tsx
+++ client/src/Pages/Welcome/Welcome.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
+import Header from "../../Components/Header";
+import Subheader from "../../Components/Subheader";
+import Button from "../../Components/Button";
+
 const Welcome: React.FC = () => {
   return (
-    <div>Welcome</div>
+    <div>
+      <div className="main">
+        <Header text="Welcome to Users Module" />
+        <Subheader text="Existing Users" />
+        <Button name="login" text="Login" variant="light" link="./login" />
+        <Subheader text="New Users" />
+        <Button
+          name="register"
+          text="Register"
+          variant="light"
+          link="./register"
+        />
+        {/* {state && state.logout && <Logout />} */}
+      </div>
+    </div>
   )
 }
 
