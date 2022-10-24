edbd3bdeecaf5f54bde3502d7191ec0bfa200a61
Rofel <jhanrofel@gmail.com>
2022-10-24 16:57:31 +0800

    remove link props for button reusable components

--- client/src/Components/Button.tsx
+++ client/src/Components/Button.tsx
@@ -1,27 +1,23 @@
 import BsButton from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
 type AppProps = {
   name: string;
   text: string;
   variant?: string;
   className?: string;
-  link: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-const Button = ({ name, text, variant, className, link, onClick }: AppProps) => {
+const Button = ({ name, text, variant, className, onClick }: AppProps) => {
   return (
     <>
-      <Link to={link}>
-        <BsButton
-          name={name}
-          variant={variant}
-          className={className}
-          onClick={onClick}
-        >
-          {text}
-        </BsButton>
-      </Link>
+      <BsButton
+        name={name}
+        variant={variant}
+        className={className}
+        onClick={onClick}
+      >
+        {text}
+      </BsButton>
     </>
   )
 }
